a6cf347cf0080e054f50d99058009ddc5facb2e8
marcsello <punkosdmarcell@rocketmail.com>
2021-03-11 21:56:14 +0100

    Added upload handling for multiple files

--- timecapsule/views/upload.py
+++ timecapsule/views/upload.py
@@ -8,15 +8,16 @@ from flask_classful import FlaskView
 import hashlib
 
 from model import db, Upload
-from schemas import UploadSchema
+from schemas import UploadSchema, FileSchema
 from utils import rechaptcha_required, apikey_required, form_required
 from marshmallow.exceptions import ValidationError
 
 
 class UploadView(FlaskView):
-    uploads_schema_simple = UploadSchema(many=True, exclude=['text', 'attachment_original_filename', 'attachment_hash'])
-    upload_schema_simple = UploadSchema(many=False, exclude=['text', 'attachment_original_filename', 'attachment_hash'])
+    uploads_schema_simple = UploadSchema(many=True, exclude=['text', 'files'])
+    upload_schema_simple = UploadSchema(many=False, exclude=['text', 'files'])
     upload_schema_full = UploadSchema(many=False)
+    file_schema_full = FileSchema(many=False)
 
     @rechaptcha_required
     @form_required
@@ -28,38 +29,52 @@ class UploadView(FlaskView):
         except ValidationError as e:
             return abort(422, str(e))  # This is not a return actually, I just want PyCharm to shut up
 
-        # Handle attachement if needed
-        if 'attachment' in request.files:
-            attachment = request.files['attachment']
+        db.session.add(u)
 
-            attachment_original_filename = secure_filename(os.path.basename(attachment.filename))
-            attachment_hash = hashlib.md5(attachment.read()).hexdigest()
-            attachment_size = attachment.tell()
+        if request.files:
+            # ensure a containing directory
+            os.makedirs(current_app.config["UPLOAD_FOLDER"], 0o755, exist_ok=True)
+
+        # Handle attachments if needed
+        for field_name, attachment in request.files.items():
+
+            original_filename = secure_filename(os.path.basename(attachment.filename))
+            md5_hash = hashlib.md5(attachment.read()).hexdigest()
+            size = attachment.tell()
             # MD5 calculating read the file to the end, so we have to seek back to it's beginning to actually save it
             attachment.seek(0, 0)
 
-            # ensure a containing directory
-            target_dir = current_app.config["UPLOAD_FOLDER"]
-            os.makedirs(target_dir, 0o755, exist_ok=True)
-
             # Save file
-            target_file_extension = os.path.splitext(attachment_original_filename)[-1]
+            target_file_extension = os.path.splitext(original_filename)[-1]
             # We don't want filenames ending with a dot
             if target_file_extension == '.':
                 target_file_extension = ''
 
-            target_filename = os.path.join(target_dir, f"{attachment_hash}_{attachment_size}{target_file_extension}")
-            attachment_duplicate = os.path.isfile(target_filename)
+            target_filepath = os.path.join(
+                current_app.config["UPLOAD_FOLDER"],
+                f"{md5_hash}_{size}{target_file_extension}"
+            )
+
+            attachment_duplicate = os.path.isfile(target_filepath)
 
             if not attachment_duplicate:
-                attachment.save(target_filename)
+                attachment.save(target_filepath)
 
-            u.attachment_hash = attachment_hash
-            u.attachment_original_filename = attachment_original_filename
-            u.attachment_size = attachment_size
+            file_params = {
+                'original_filename': original_filename,
+                'md5_hash': md5_hash,
+                'size': size
+            }
 
-        # Save stuff to database
-        db.session.add(u)
+            try:
+                f = self.file_schema_full.load(file_params, session=db.session)
+            except ValidationError as e:
+                return abort(422, str(e))  # This is not a return actually, I just want PyCharm to shut up
+
+            f.upload = u
+            db.session.add(f)
+
+        # Commit all that stuff to database
         db.session.commit()
 
         return jsonify(self.upload_schema_simple.dump(u)), 201
