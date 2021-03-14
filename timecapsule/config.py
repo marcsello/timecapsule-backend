cb9789c0d7bbc501c7a2d31eada4bf105342d14f
marcsello <punkosdmarcell@rocketmail.com>
2021-03-14 15:26:49 +0100

    disabled modification tracking

--- timecapsule/config.py
+++ timecapsule/config.py
@@ -8,6 +8,7 @@ Configuration
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI", "sqlite://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(12))
     CORS_ORIGINS = os.environ.get("ALLOWED_ORIGINS", "*")
 
