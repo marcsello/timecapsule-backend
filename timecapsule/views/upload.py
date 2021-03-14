111bebff731cf51aa8299cc384e55f0d25d8e7f0
marcsello <punkosdmarcell@rocketmail.com>
2021-03-14 15:26:17 +0100

    Refactored upload flow

--- timecapsule/views/upload.py
+++ timecapsule/views/upload.py
@@ -32,71 +32,72 @@ class UploadView(FlaskView):
 
         db.session.add(u)
 
-        # Start handling of files
+        # Start handling of files only if there is any
         if request.files:
-            # ensure a containing directory
+
+            if len(request.files) > current_app.config['MAX_FILES_IN_UPLOAD']:
+                return abort(422, "Too many files")
+
+            # Handle attachments if needed
+            files_to_save = []
+            m = Magic(mime=True)
+            for field_name, attachment in request.files.items():
+
+                original_filename = secure_filename(os.path.basename(attachment.filename))
+                md5_hash = hashlib.md5(attachment.read()).hexdigest()
+                size = attachment.tell()
+
+                if size > current_app.config['MAX_SINGLE_FILE_LENGTH']:
+                    return abort(413, f"{field_name} is too big")
+
+                # MD5 calculating read the file to the end, so we have to seek back to it's beginning to actually save it
+                attachment.seek(0, 0)
+
+                # Apparently .doc requires to be fully read in order to being identified properly
+                mime = m.from_buffer(attachment.read())
+                attachment.seek(0, 0)
+
+                # prepare file to be saved
+                target_file_extension = os.path.splitext(original_filename)[-1]
+                # We don't want filenames ending with a dot
+                if target_file_extension == '.':
+                    target_file_extension = ''
+
+                target_filepath = os.path.join(
+                    current_app.config["UPLOAD_FOLDER"],
+                    f"{md5_hash}_{size}{target_file_extension}"
+                )
+
+                # Files will be saved later, after all validated successfully
+                files_to_save.append((attachment, target_filepath))
+
+                # Store meta data
+
+                file_params = {
+                    'original_filename': original_filename,
+                    'md5_hash': md5_hash,
+                    'size': size,
+                    'mime': mime
+                }
+
+                try:
+                    f = self.file_schema_full.load(file_params, session=db.session)
+                except ValidationError as e:
+                    return abort(422, f"{field_name}: {e}")  # This is not a return actually, I just want PyCharm to shut up
+
+                f.upload = u
+                db.session.add(f)
+
+            # before saving, ensure a containing directory
             os.makedirs(current_app.config["UPLOAD_FOLDER"], 0o755, exist_ok=True)
 
-        if len(request.files) > current_app.config['MAX_FILES_IN_UPLOAD']:
-            return abort(422, "Too many files")
+            # All single validation succeeded so far
+            # But database constraints are not yet enforced
+            # We just save the files here, so that if saving fails, the data won't be committed to the db
 
-        # Handle attachments if needed
-        files_to_save = []
-        m = Magic(mime=True)
-        for field_name, attachment in request.files.items():
-
-            original_filename = secure_filename(os.path.basename(attachment.filename))
-            md5_hash = hashlib.md5(attachment.read()).hexdigest()
-            size = attachment.tell()
-
-            if size > current_app.config['MAX_SINGLE_FILE_LENGTH']:
-                return abort(413, f"{field_name} is too big")
-
-            # MD5 calculating read the file to the end, so we have to seek back to it's beginning to actually save it
-            attachment.seek(0, 0)
-
-            # Apparently .doc requires to be fully read in order to being identified properly
-            mime = m.from_buffer(attachment.read())
-            attachment.seek(0, 0)
-
-            # prepare file to be saved
-            target_file_extension = os.path.splitext(original_filename)[-1]
-            # We don't want filenames ending with a dot
-            if target_file_extension == '.':
-                target_file_extension = ''
-
-            target_filepath = os.path.join(
-                current_app.config["UPLOAD_FOLDER"],
-                f"{md5_hash}_{size}{target_file_extension}"
-            )
-
-            # Files will be saved later, after all validated successfully
-            files_to_save.append((attachment, target_filepath))
-
-            # Store meta data
-
-            file_params = {
-                'original_filename': original_filename,
-                'md5_hash': md5_hash,
-                'size': size,
-                'mime': mime
-            }
-
-            try:
-                f = self.file_schema_full.load(file_params, session=db.session)
-            except ValidationError as e:
-                return abort(422, f"{field_name}: {e}")  # This is not a return actually, I just want PyCharm to shut up
-
-            f.upload = u
-            db.session.add(f)
-
-        # All single validation succeeded so far
-        # But database constraints are not yet enforced
-        # We just save the files here, so that if saving fails, the data won't be committed to the db
-
-        for attachment, target_filepath in files_to_save:
-            if not os.path.isfile(target_filepath):
-                attachment.save(target_filepath)
+            for attachment, target_filepath in files_to_save:
+                if not os.path.isfile(target_filepath):
+                    attachment.save(target_filepath)
 
         # Commit all that stuff to database
         db.session.commit()
